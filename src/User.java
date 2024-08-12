7dab7cd456d8da7ffc3d83eeef11ee822c818c25
HarleyBishop <Harley.bishop@connect.qut.edu.au>
2024-08-12 13:45:39 +1000

    1st user done

--- src/User.java
+++ src/User.java
@@ -1,11 +1,13 @@
+import java.util.ArrayList;
+
 public class User {
     private String username;
     private String password;
 
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
+    //public User(String username, String password) {
+     //   this.username = username;
+     //   this.password = password;
+   // }
 
     /**
      * Gets the username of the user.
@@ -23,6 +25,7 @@ public class User {
 
     /**
      * Sets the username of the user.
+     *
      * @param username The username to set.
      */
     public void setUsername(String username) {
@@ -31,9 +34,28 @@ public class User {
 
     /**
      * Sets the password of the user.
+     *
      * @param password The password to set.
      */
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    // ... (other fields and methods)
+
+    private ArrayList<ToDoItem> toDoItems;
+
+    public User(String username, String password) {
+        // ... (other initializations)
+        this.toDoItems = new ArrayList<>();
+    }
+
+    /**
+     * Gets the to-do items of the user.
+     *
+     * @return A reference to the list of to-do items, which can be modified from outside the class.
+     */
+    public ArrayList<ToDoItem> getToDoItems() {
+        return toDoItems;
     }
 }
